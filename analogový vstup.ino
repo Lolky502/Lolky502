431070c8142178a025010c481ceb684e393caef8
Jardak <kutalek@gymlit.cz>
2023-05-09 15:09:07 +0200

    Tlačítko jste neupravil podle zadání Máte binec v adresářích, udělejte v tom pořádek

--- analogový vstup.ino
+++ analogový vstup.ino
@@ -1,10 +1,18 @@
+// Zkopírujte minulý program, ať nezačínáte od nuly
+// nastavte parametry pro jednotlivé piny
+//         analogový vstupní
+//         digitální pro led červenou, žlutou a zelenou
+
 void setup() {
   // put your setup code here, to run once:
   
+  // nastavte vstupní analogový pin a výstupní 
+ 
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+ // otestujeho velikost a rozsviťte odpovídající diodu
 }
